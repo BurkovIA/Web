042d39c41930662e249e86812e613a6f2e96b448
BurkovIA <burkov88@mail.ru>
2024-07-26 08:36:49 +0800

    request

---
+++ src/main/java/org/example/Request.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Request {
+    public static String process(String message) {
+        // Add your request processing logic here
+        return "Processed message: " + message;
+    }
+}
